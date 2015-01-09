91023ee703fe7956e77196813953b865830491e1
ezekg <ezekg@yahoo.com>
2015-01-09 10:49:12 -0600

    add ability to pass array of pages to related func

--- theme-juice/functions/page-is-related-to.php
+++ theme-juice/functions/page-is-related-to.php
@@ -1,22 +1,37 @@
 <?php
 
 /**
- * Checks if page is $slug, or if it is related to $slug
+ * Checks if page is $slug, or if it is related to $slugs
  *
- * @param {String} $slug - Slug of page to check
+ * @param {Array|String} $slugs - Single slug or array of slugs of pages
  *
  * @return {Bool}
  */
-function tj_page_is_related_to( $slug ) {
+function tj_page_is_related_to( $slugs ) {
     global $post;
 
-    // Get page by slug
-    $page = get_page_by_path( $slug );
-
-    // Check if it's valid
-    if ( $page ) {
-        return ( is_page( $slug ) || "$page->ID" === "$post->post_parent" );
-    } else {
-        return false;
+    // Check if we're dealing with an array of pages
+    if ( ! is_array( $slugs ) ) {
+        $slugs = array( $slugs );
     }
+
+    foreach ( $slugs as $slug ) {
+        echo $slug;
+
+        // Get page by slug
+        $page = get_page_by_path( $slug );
+
+        // Check if it's a valid page
+        if ( $page ) {
+
+            // Check if page is related to $slug
+            $result = ( is_page( $slug ) || "$page->ID" === "$post->post_parent" );
+
+            if ( $result ) {
+                return $result;
+            }
+        }
+    }
+
+    return false;
 }
