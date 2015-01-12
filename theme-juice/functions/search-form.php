76cef1a326bf95f14703ef0bb7d866cc3614db3d
ezekg <ezekg@yahoo.com>
2015-01-12 17:19:24 -0600

    update search form

--- theme-juice/functions/search-form.php
+++ theme-juice/functions/search-form.php
@@ -4,17 +4,18 @@
  * Render search form
  *
  * @param {Bool}   $display     - Automatically echo output
+ * @param {String} $button      - Text inside of submit button
  * @param {String} $placeholder - Placeholder text inside of field
  *
  * @return {String}
  */
-function tj_search_form( $display = true, $placeholder = "Search" ) {
+function tj_search_form( $display = true, $button = "Search", $placeholder = "Search" ) {
     $buffer = array();
 
     $buffer[] = "<form role='search' method='get' class='search-field__wrapper' action='" . home_url() . "' >";
     $buffer[] = "<div class='search-field'>";
     $buffer[] = "<input class='search-field__input' type='text' value='" . get_search_query() . "' name='s' id='s' placeholder='$placeholder' />";
-    $buffer[] = "<input class='search-field__submit' type='submit' value='". esc_attr__( "Search" ) ."' />";
+    $buffer[] = "<input class='search-field__submit' type='submit' value='$button' />";
     $buffer[] = "</div>";
     $buffer[] = "</form>";
 
