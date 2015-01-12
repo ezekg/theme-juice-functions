83118d0c75f15efe5769b52801bba752cb02e559
ezekg <ezekg@yahoo.com>
2015-01-12 09:41:28 -0600

    update excerpt func

--- theme-juice/functions/excerpt.php
+++ theme-juice/functions/excerpt.php
@@ -10,9 +10,8 @@
  */
 function tj_excerpt( $count = 42, $display = true ) {
 
-    $content = strip_tags( do_shortcode( get_the_content() ) );
-    $output = implode( "", array_slice( preg_split( "/([\s,\.;\?\!]+)/", $content,  ( $count * 2 + 1 ), PREG_SPLIT_DELIM_CAPTURE ), 0, ( $count * 2 - 1 ) ) );
-    $output .= str_word_count( $content ) > $count ? "&hellip;" : "";
+    // Trim content after shortcodes are rendered
+    $output = wp_trim_words( do_shortcode( get_the_content() ), $count );
 
     // Echo outout if $display
     if ( $display ) {
